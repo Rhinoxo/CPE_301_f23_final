fbfb6a65e3bc74ba4e7efc224230112fdee3a070
1104-Rangel-Peter <97935966+1104-Rangel-Peter@users.noreply.github.com>
2023-12-13 12:26:28 -0800

    Update project.ino

--- project.ino
+++ project.ino
@@ -6,10 +6,20 @@
 #define OFFLED 12
 #define ERRLED 13
 
+#define lcdRS 8
+#define lcdEN 7
+#define lcdD4 6
+#define lcdD5 5
+#define lcdD6 4
+#define lcdD7 3
+
 #define tempSensor 0
+#define waterSensor 5
 
 #define waterLimit 10
-#define tempLimit 10
+#define tempLimit 150
+
+#include <LiquidCrystal.h>
 
 int waterLevel = 0;
 int tempLevel = 0;
@@ -18,6 +28,8 @@ bool active = false;
 
 enum states {IDLE, RUN, OFF, ERR};
 int state = OFF;
+
+LiquidCrystal lcd(lcdRS, lcdEN, lcdD4, lcdD5, lcdD6, lcdD7);
 
 void setup(){
   Serial.begin(9600);
@@ -28,13 +40,19 @@ void setup(){
   pinMode(RUNLED, OUTPUT);
   pinMode(OFFLED, OUTPUT);
   pinMode(ERRLED, OUTPUT);
+  
+  lcd.begin(16,2);
 
   pinMode(powerButton, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(powerButton), powerToggle, RISING);
 }
 
 void loop(){
-  delay(10);
+  delay(1000);
+  lcd.clear();
+  
+  tempLevel = analogRead(tempSensor);
+  waterLevel = analogRead(waterSensor);
 
   if(state == IDLE || state == RUN){
     if(waterLevel <= waterLimit){state = ERR;}
@@ -51,16 +69,24 @@ void loop(){
   switch(state){
     case IDLE:
       IDLELEDstate = HIGH;
+      lcd.setCursor(0,1);
+      lcd.print(tempLevel);
       break;
     case RUN:
       RUNLEDstate = HIGH;
       fanPINstate = HIGH;
+      lcd.print("Running");
+      lcd.setCursor(0,1);
+      lcd.print(tempLevel);
       break;
     case OFF:
       OFFLEDstate = HIGH;
       break;
     case ERR:
       ERRLEDstate = HIGH;
+      lcd.print("ERROR!");
+      lcd.setCursor(0,1);
+      lcd.print("Water too low!");
       break;
   }
 
@@ -76,4 +102,5 @@ void powerToggle(){
 
   if(active && state != RUN){state = IDLE;}
   else if(!active){state = OFF;}
+  loop();
 }
